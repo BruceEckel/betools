1d697c43e13df03d1e0725ea2a2b140b6d5e5d2d
Bruce Eckel <BruceTEckel@gmail.com>
2015-05-11 21:08:09 -0700

    Changed to relative imports

--- betools/__init__.py
+++ betools/__init__.py
@@ -1,6 +1,6 @@
 # https://github.com/pypa/sampleproject/blob/master/setup.py
 
 __all__ = ["CmdLine", "visitDir", "ruler", "head"]
-from betools.cmdline import CmdLine
-from betools.visitdir import visitDir
-from betools.ruler import ruler, head
+from .cmdline import CmdLine
+from .visitdir import visitDir
+from .ruler import ruler, head
