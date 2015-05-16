d8794be78567ffc1265e487de8bcfa9933e5ba5d
Bruce Eckel <mindviewinc@gmail.com>
2015-05-16 12:11:45 -0700

    Include output

--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -1,10 +1,10 @@
 # Test the local one, not the installed one:
 import sys
 sys.path.insert(0, "..")
-from betools import CmdLine
 # CmdLine appears to be global for all tests,
 # so you can't use the same letter flag throughout
 # the test suite (is this a py.test bug?)
+from betools import CmdLine
 
 @CmdLine("1", "option1")
 def test_1():
@@ -30,21 +30,21 @@ def test_3():
 @CmdLine("4")
 def option4():
     """
-    Use the function name as the word tag.
+    Option 4 uses the function name as the word tag.
     """
     print("option4")
 
 @CmdLine("5")
 def option5():
     """
-    Use the function name as the word tag.
+    Option 5 uses the function name as the word tag.
     """
     print("option5")
 
 @CmdLine("6")
 def option6():
     """
-    Use the function name as the word tag.
+    Option 6 uses the function name as the word tag.
     """
     print("option6")
 
@@ -53,3 +53,16 @@ def test_main():
 
 if __name__ == '__main__':
     test_main()
+
+output = """
+usage: test_cmdline.py [-h] [-1] [-2] [-3] [-4] [-5] [-6]
+
+optional arguments:
+  -h, --help     show this help message and exit
+  -1, --option1  Description of option1
+  -2, --option2  Description of option2
+  -3, --option3  Description of option3
+  -4, --option4  Option 4 uses the function name as the word tag.
+  -5, --option5  Option 5 uses the function name as the word tag.
+  -6, --option6  Option 6 uses the function name as the word tag.
+"""
